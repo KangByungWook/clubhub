450902e670012e532e128ebf1c044f161cba5ed7
KangByungWook <brillante91@naver.com>
2015-08-20 15:33:12 +0000

    article_modal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
-before_action :authenticate_user!, only: [:index]
-
+before_action :authenticate_user!, only: [:index, :index_write]
+    
     def index
+        @contents=Article.all.reverse
         
+    end
+    
+    def index_write
+       a=Article.new
+       a.author=current_user.name
+       a.title=params[:title]
+       a.content=params[:content]
+       a.save
+       redirect_to :back
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817044600) do
+ActiveRecord::Schema.define(version: 20150820143704) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "author"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
